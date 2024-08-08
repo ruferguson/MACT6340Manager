82d43b3cf1827d9793624c87fd32b453f3425862
ruferguson <ruf@smu.edu>
2024-08-07 22:31:30 -0700

    added app.js, main.js, and projectMeta.js

---
+++ build/1-project-bundle/projectMeta.js
@@ -0,0 +1,20 @@
+export const projectInfo = {
+  projectName: "dotscillate",
+  artistsPayoutWallet: "0x97991F5f4803d23ddC1076B0cAf89925c03067a5",
+  numberOfEditions: 13,
+  releaseDate: "2024-08-08 10:00:00",
+  websiteProjectDescription:
+    "Points in a elliptical shape oscillate organically to draw a pattern.  The color and exact pattern of the dots is determined by each unique hash.",
+  price: 0.01,
+  royaltiesPercent: 5.0,
+  titleForViewport: "Organically oscillating dot design in P5.js",
+  projectWebUrl: "https://mact6340-app-qtq3i.ondigitalocean.app/projects",
+  openSeaCollectionName: "dotscillate",
+  openSeaCollectionDescription:
+    "A pattern of oscillating dots rendered with P5.js",
+  openSeaCollectionImage: "",
+  openSeaCollectionSeller_fee_basis_points: 500,
+  openSeaCollectionFee_recipient: "0x97991F5f4803d23ddC1076B0cAf89925c03067a5",
+  tokenDescriptionText:
+    "One of the exquisite visual designs of the dotscillate collection.",
+};
